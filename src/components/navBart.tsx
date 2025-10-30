9a286830ffd0d2033b2b82711f45a95eb8183e01
sobhan jafari <149336338+sobhanjaferi@users.noreply.github.com>
2025-10-30 10:10:46 +0330

    Edit Navbar

--- src/components/navBart.tsx
+++ src/components/navBart.tsx
@@ -78,23 +78,23 @@ function NavBar({ position }: { position: string }) {
 
   return (
     <nav
-      className={`flex justify-between items-center h-20
-      py-10 px-8 z-100 transition-all
+      className={`flex justify-between items-center h-10
+      py-8 px-5 z-100 transition-all
       duration-700 ease-in-out
       ${position} ${scroll ? "bg-white shad opacity-100" : ""}`}
     >
-      <h1 className="text-3xl text-blue-950 font-bold">BizBud</h1>
+      <h1 className="text-3xl text-blue-950 font-bold sm:ml-[-5px]">BizBud</h1>
 
-      <div className="fixed right-7.5 top-7.5 flex justify-center items-center">
+      <div className="fixed right-7.5 top-6 flex justify-center items-center">
         <i
-          className={`cursor-pointer fa-solid fa-list block lg:opacity-0 transition-all duration-400 ease-in-out ${
+          className={`cursor-pointer fa-solid fa-list block lg:opacity-0 sm:opacity-0 transition-all duration-400 ease-in-out ${
             navList ? "rotate-180 text-violet-600" : "rotate-0"
           }`}
           onClick={handleOpenClose}
         ></i>
       </div>
       <div
-        className={`sm:flex sm:flex-col sm:justify-center flex flex-col justify-center items-center lg:flex lg:justify-center lg:items-center lg:flex-row lg:mt-0 lg:mr-[-30px] transition-all duration-700 ease-in-out lg:opacity-0 ${
+        className={`flex flex-col justify-center items-center lg:flex lg:justify-center lg:items-center lg:flex-row lg:mt-0 lg:mr-[-30px] sm:flex sm:justify-center sm:items-center sm:flex-row sm:mt-0 sm:mr-[-30px] transition-all duration-700 ease-in-out lg:opacity-0 ${
           timer ? "lg:opacity-100 lg:mr-[0px]" : ""
         }  ${
           navList
@@ -104,11 +104,11 @@ function NavBar({ position }: { position: string }) {
           scroll ? "bg-white" : ""
         } lg:mr-0 lg:rounded-none lg:p-0 mt-75 mr-0 rounded-xl p-2`}
       >
-        <ul className="sm:flex sm:justify-between sm:items-center sm:flex-col lg:mr-2 lg:flex-row flex flex-col justify-center items-center">
+        <ul className="lg:flex-row sm:mr-2 sm:flex-row flex flex-col justify-center items-center">
           {navItems.map((item: TnavItems) => (
             <Link key={item.id} href={item.link}>
               <li
-                className={`mx-3 transition-all duration-500 ease-in-out hover:text-violet-600 ${
+                className={`lg:mr-5 sm:mr-2 transition-all duration-500 ease-in-out hover:text-violet-600 ${
                   pagePath == item.link ? "text-violet-600" : ""
                 }`}
               >
@@ -118,7 +118,7 @@ function NavBar({ position }: { position: string }) {
           ))}
         </ul>
 
-        <Button name="Call Now" newStyle="lg:mt-0 sm:px-7 sm:py-2" />
+        <Button name="Call Now" newStyle="lg:mt-0 lg:px-7 lg:py-2 lg:mr-0 sm:mt-0 sm:px-4 sm:py-1 sm:mr-3" />
       </div>
     </nav>
   );

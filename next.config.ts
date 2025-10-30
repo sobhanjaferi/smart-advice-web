15eb102d70dfb49d7e4bcb358c640fdf075d125a
sobhan jafari <149336338+sobhanjaferi@users.noreply.github.com>
2025-10-30 11:06:52 +0330

    github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // برای ساخت خروجی استاتیک
+  images: {
+    unoptimized: true, // برای جلوگیری از خطا در GitHub Pages
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ نادیده گرفتن ESLint هنگام build
+  },
+  basePath: '/smart-advice-web', // 👈 نام ریپوی گیت‌هابت با اسلش ابتدایی
+  assetPrefix: '/smart-advice-web/', // 👈 همان نام ریپو با اسلش ابتدایی و انتهایی
+  trailingSlash: true, // برای سازگاری بهتر با GitHub Pages
 };
 
 export default nextConfig;

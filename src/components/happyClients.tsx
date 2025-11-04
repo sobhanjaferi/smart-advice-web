075c7f312c79477230cf05cd0ae178747a15fdfa
sobhan jafari <149336338+sobhanjaferi@users.noreply.github.com>
2025-11-04 09:41:01 +0330

    respansive client section

--- src/components/happyClients.tsx
+++ src/components/happyClients.tsx
@@ -1,6 +1,6 @@
 function HappyClients() {
   return (
-    <div className="w-full h-50">
+    <div className="w-full">
       <div className="bg-gray-300 flex justify-center items-end h-25 p-3 text-blue-950 font-bold">
         OUR HAPPY CLIENTS
       </div>
@@ -8,21 +8,27 @@ function HappyClients() {
       <hr className="text-white" />
       <hr className="text-white" />
 
-      <div className="bg-gray-300 h-25 flex justify-around items-center">
-        <div className="text-white text-xl">
+      <div className="bg-gray-300 flex lg:flex-row sm:flex-row flex-col justify-around items-center">
+        <div className="text-white lg:text-xl sm:text-lg text-xl mt-10 pb-3 border-b-3 border-white">
           <span className="p-2 bg-blue-950 font-bold mr-[5px]">BAR</span>
           <span className="p-2 bg-blue-950 font-bold">121</span>
         </div>
 
-        <div className="text-2xl text-blue-950">H U N T E R</div>
+        <div className="lg:text-2xl sm:text-lg text-2xl text-blue-950 mt-10 border-b-3 border-white">
+          H U N T E R
+        </div>
 
-        <div className="font-extrabold text-2xl text-blue-950 font-serif">
+        <div className="font-extrabold lg:text-2xl sm:text-lg text-2xl text-blue-950 font-serif mt-10 border-b-3 border-white">
           Loco Graphix
         </div>
 
-        <div className="text-2xl text-blue-950 font-bold">Mid Dreams</div>
+        <div className="lg:text-2xl sm:text-lg text-2xl text-blue-950 font-bold mt-10 border-b-3 border-white">
+          Mid Dreams
+        </div>
 
-        <div className="text-2xl text-blue-950 font-bold">DAVE JONES</div>
+        <div className="lg:text-2xl sm:text-lg text-2xl text-blue-950 font-bold mt-10 border-b-3 border-white">
+          DAVE JONES
+        </div>
       </div>
     </div>
   );
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,13 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 function Slider() {
+  const [scroll, setScroll] = useState<Boolean>(false);
+  const [scrollMobile, setScrollMobile] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const HandleScroll = () => {
+      if (window.scrollY >= 2300) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", HandleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", HandleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const HandleScrollMobile = () => {
+      if (window.scrollY >= 3200) {
+        setScrollMobile(true);
+      } else {
+        setScrollMobile(false);
+      }
+    };
+
+    window.addEventListener("scroll", HandleScrollMobile);
+
+    return () => {
+      window.removeEventListener("scroll", HandleScrollMobile);
+    };
+  }, []);
+
   return (
     <>
       <div className="w-full h-150 bg-violet-600 text-white">
         {/* ================ slide one ================ */}
 
         <div className="w-full h-9/10 flex flex-col justify-around items-center">
-          <h2 className="tracking-widest">TESTIMONIALS</h2>
+          <h2
+            className={`tracking-widest opacity-0 translate-y-[-50px] transition-all duration-1000 ${
+              scroll ? "opacity-100 translate-y-[0px]" : ""
+            }`}
+          >
+            TESTIMONIALS
+          </h2>
 
-          <p className="font-bold text-3xl sm:text-4xl lg:text-5xl text-center">
+          <p
+            className={`font-bold text-3xl sm:text-4xl lg:text-5xl text-center translate-x-[-100px] opacity-0 transition-all duration-1200 ${
+              scroll ? "opacity-100 translate-x-[0]" : ""
+            }`}
+          >
             “I'm a testimonial. Click to <br /> edit me and add text”
           </p>
 
